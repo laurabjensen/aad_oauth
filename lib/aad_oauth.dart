7ddd0c85d25a46a36a3c39cd8f1a24b7fbac7b17
laurabjensen <laura.jensen@outlook.com>
2022-07-01 16:00:13 +0200

    export exception

--- lib/aad_oauth.dart
+++ lib/aad_oauth.dart
@@ -6,6 +6,8 @@ import 'package:aad_oauth/helper/core_oauth.dart';
 import 'model/config.dart';
 import 'dart:async';
 
+export 'model/exception.dart';
+
 /// Authenticates a user with Azure Active Directory using OAuth2.0.
 class AadOAuth {
   final CoreOAuth _coreOAuth;
